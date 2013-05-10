3ac6c393734047c6da69de61f8829925a164b408
Jake Berendes <jacobberendes@gmail.com>
2013-05-09 21:25:41 -0500

    update helper js

--- assets/websource/source/js/mylibs/helper.js
+++ assets/websource/source/js/mylibs/helper.js
@@ -20,6 +20,9 @@ $(".btn").click(function(){
 	activity.TestMethod();
 });
 
+MBP.updateContent = function(sometext){
+    $('.content').text(sometext);
+}
 // Fix for iPhone viewport scale bug 
 // http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
 
